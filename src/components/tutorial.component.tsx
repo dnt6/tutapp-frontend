dee58feee75d71de022b0a20c8b0628a61d14392
Dhruv T <dhruv.thota@fiserv.com>
2023-12-19 12:05:25 -0500

    initializing repo...

---
+++ src/components/tutorial.component.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Tutorial extends Component {
+    render() {
+        return (
+            <div>
+                Hi
+            </div>
+        )
+    }
+
+}
+
+export default Tutorial;
---
+++ src/components/tutorials-list.component.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TutorialsList extends Component {
+    render() {
+        return (
+            <div>
+                Hi
+            </div>
+        )
+    }
+}
+
+export default TutorialsList;
